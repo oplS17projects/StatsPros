dadf7964511523279394219c6e2258a9efb775f3
daniel.ditommaso <daniel.ditommaso@kronos.com>
2017-04-05 11:23:56 -0400

    Cached api responses for Players/Teams to reduce deployment time.

--- src/DataService.rkt
+++ src/DataService.rkt
@@ -4,19 +4,48 @@
 (require net/url)
 (require json)
 
+#|
+For dev purposes leave this set to #t.
+Otherwise DataService will request all fresh data from NBA API, which takes some time.
+|#
+(define offlineMode #t)
+
 (define (retrieveData url)
   (define in (get-pure-port url))
   (define response-string (port->string in))
   (close-input-port in)
   (if (jsexpr? response-string) (string->jsexpr response-string) (error (string-append "Invalid query: " response-string))))
 
+;;TO-DO Now that storing responses is possible, implement some form of caching for 'live' use, rather than strictly offline.
+  ;(define out (open-output-file "data"))
+  ;(display response-string out)
+  ;(close-output-port out)
+
 (define (retrieveTeamsList)
-  (define in (get-pure-port (string->url "http://stats.nba.com/stats/commonTeamYears/?LeagueID=00")))
-  (define response-string (port->string in))
+  (if offlineMode (offlineTeams) (onlineTeams)))
+
+(define (offlineTeams)
+  (define in (open-input-file "teamsListData"))
+  (define response-string (read-line in))
   (close-input-port in)
   (if (jsexpr? response-string) (parseResponse response-string) (error (string-append "Invalid query: " response-string))))
 
+(define (onlineTeams)
+  (define in (get-pure-port (string->url "http://stats.nba.com/stats/commonTeamYears/?LeagueID=00")))
+  (define response-string (port->string in))
+  (close-input-port in)
+  (if (jsexpr? response-string) (parseResponse response-string) (error (string-append "Invalid query: " response-string)))) 
+
 (define (retrievePlayersList)
+  (if offlineMode (retrieveOfflinePlayers) (retrieveOnlinePlayers)))
+
+(define (retrieveOfflinePlayers)
+  (define in (open-input-file "playerListData"))
+  (define response-string (read-line in))
+  (close-input-port in)
+  (if (jsexpr? response-string) (parseResponse response-string) (error (string-append "Invalid query: " response-string))))
+
+(define (retrieveOnlinePlayers)
   (define in (get-pure-port (string->url "http://stats.nba.com/stats/commonAllPlayers/?Season=2016-17&IsOnlyCurrentSeason=1&LeagueID=00")))
   (define response-string (port->string in))
   (close-input-port in)
