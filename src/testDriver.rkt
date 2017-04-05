6d767ed1603768d25f1e756a293611d5667608d5
daniel.ditommaso <daniel.ditommaso@kronos.com>
2017-04-05 01:12:23 -0400

    Worked out an API call that retrieves some data based on PlayerID

--- src/testDriver.rkt
+++ src/testDriver.rkt
@@ -19,22 +19,22 @@
 
 (define allTeamsRequest (string->url "http://stats.nba.com/stats/commonTeamYears/?LeagueID=00"))
 (define response (retrieveData allTeamsRequest))
-
 (define allPlayersRequest (string->url "http://stats.nba.com/stats/commonAllPlayers/?Season=2016-17&IsOnlyCurrentSeason=1&LeagueID=00"))
 (define playerResponse (retrieveData allPlayersRequest))
 
+(define someStats (retrieveData (string->url "http://stats.nba.com/stats/playerdashboardbylastngames/?MeasureType=Base&PerMode=Totals&PlusMinus=Y&PaceAdjust=N&Rank=N&Season=2015-16&SeasonType=Regular Season&PlayerID=201565&Outcome=&Location=&Month=1&SeasonSegment=&DateFrom=&DateTo=&OpponentTeamID=0&VsConference=&VsDivision=&GameSegment=&Period=0&LastNGames=0")))
+  
 (define responseHash (car (hash-ref response 'resultSets)))
-(define listofTeams (hash-ref responseHash 'rowSet))
-
 (define playerHash (car (hash-ref playerResponse 'resultSets)))
+(define listofTeams (hash-ref responseHash 'rowSet))
 (define listofPlayers (hash-ref playerHash 'rowSet))
 
 
 ;;Search for a players entry in listofPlayers. IE "Rose, Derrick"
-(define (findPlayerEntry2 name)
+(define (findPlayerEntry2 name listofPlayers)
   (filter (lambda (x) (equal? (list-ref x 1) name)) listofPlayers))
 ;;Retreive list of player entries by team Abrv. IE "Cleveland" = "CLE"
-(define (findPlayersOnTeam teamAbv)
+(define (findPlayersOnTeam teamAbv listofPlayers)
   (filter (lambda (x) (equal? (list-ref x 10) teamAbv)) listofPlayers))
 
                          
@@ -43,6 +43,9 @@
 
 ;;TO-DO Test and Build one more Call to API service that we will be begin using for 'analysis'.
 ;;      This should be all we need to set up a basic 'hand-off' between PK and I to begin work on our individual pieces.
+;;         --Update: someStats is a working call w/ relevant data based on a playerID. We should start here.
+;;                   there are quite a few flags to set, so this call will require tweaking.
+
 ;;      Current state of code reflects nice base for configuring select boxes in a webserver instance of racket.
 ;;      IF THERE IS TIME. We should configure a way to dump the initial testing response of the API to avoid having re-fetch data we haven't changed upon re-running.
 
