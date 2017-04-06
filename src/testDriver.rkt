c3db2e043e0e428a36c9c2032c4b7ddd41219700
daniel.ditommaso <daniel.ditommaso@kronos.com>
2017-04-06 05:26:42 -0400

    Finished configuring the webserver. Should be smooth sailing from here.

--- src/testDriver.rkt
+++ src/testDriver.rkt
@@ -10,38 +10,43 @@
 (define selectTeam
   (map (lambda(x) (dropdownEntry x)) (retrieveTeamAbrv)))
 (define selectPlayer
-  (list (dropdownEntry "XXXXXXXXX")))
-#|
-(define (start request)
-  (response/xexpr
-   '(html
-     (head (title "My Application"))
-     (body (select (option "XXX")
-                   (option "YYz"))))))
-|#
-;(static-files-path "htdocs")
-(define (start request)
-  (render-blog-page selectTeam request))
+  (list (dropdownEntry "")))
 
-(define (render-blog-page blog request)
-  (local [(define (response-generator make-url)
-            (response/xexpr
-             `(html (head (title "StatsPros"))
-                    (body (h1 "Please Select Team: ")
-                          (form ((action
-                                  ,(make-url insert-dropdown-handler)))
-                                ,(render-dropdownEntrys blog)
-                                (input ((type "submit"))))
-                          ))))
-          (define (insert-dropdown-handler request)
-            (render-blog-page
-             (map (lambda (x) (dropdownEntry x))(findPlayersOnTeam (extract-binding/single 'teamdropdown (request-bindings request))))
-             request))]
-    (send/suspend/dispatch response-generator)))
+(define (start request)
+  (render-StatsPros-page selectTeam request))
 
-(define (render-dropdownEntrys blog)
-  `(select ((name "teamdropdown"))
-           ,@(map render-dropdownEntry blog)))
+(define (render-StatsPros-page teamDropdown request)
+  (define (response-generator make-url)
+    (response/xexpr
+     `(html (head (title "StatsPros"))
+            (body (h1 "Please select Teams: ")
+                  (form ((action
+                          ,(make-url teamDropDownHandler)))
+                        ,(render-dropdownEntrys teamDropdown "team1dropdown")
+                        ,(render-dropdownEntrys teamDropdown "team2dropdown")
+                        (input ((type "submit"))))))))
+  (define (teamDropDownHandler request)
+    (render-playerSelect-page request))
+  (send/suspend/dispatch response-generator))
+
+(define (render-playerSelect-page request)
+  (define (response-generator make-url)
+    (response/xexpr
+     `(html (head (title "StatsPros"))
+            (body (h1 "Please select Players: ")
+                  (form ((action
+                          ,(make-url playerDropDownHandler)))
+                        ,(render-dropdownEntrys (map (lambda (x) (dropdownEntry x))(findPlayersOnTeam (extract-binding/single 'team1dropdown (request-bindings request)))) "player1dropdown")
+                        ,(render-dropdownEntrys (map (lambda (x) (dropdownEntry x))(findPlayersOnTeam (extract-binding/single 'team2dropdown (request-bindings request)))) "player2dropdown")
+                        (input ((type "submit"))))
+                  ))))
+  (define (playerDropDownHandler request)
+    (render-StatsPros-page selectTeam request))
+  (send/suspend/dispatch response-generator))
+
+(define (render-dropdownEntrys dropdown id)
+  `(select ((name ,id))
+           ,@(map render-dropdownEntry dropdown)))
 
 (define (render-dropdownEntry a-dropdownEntry)
   `(option ((value ,(dropdownEntry-entry a-dropdownEntry)))
@@ -49,11 +54,6 @@
 
 (define (parse-dropdownEntry bindings)
   (dropdownEntry (extract-binding/single 'entry bindings)))
-
-;;(define listofTeams (retrieveTeamsList))
-;;(define listofPlayers (retrievePlayersList))
-;(retrievePlayerStats "2544")
-
 
 ;;Search for a players entry in listofPlayers. IE "Rose, Derrick"
 (define (findPlayerEntry2 name listofPlayers)
@@ -64,8 +64,9 @@
   (list-ref (car playerStats) 5))
                          
 ;;TO-DO 
-;;      Current state of code reflects nice base for configuring select boxes in a webserver instance of racket.
-;;      Finished basic info retrieval and data caching. Talk to PK about how to utilize DataService.
-;;      Begin setting up UI to select teams and players. For now, we will default to one hard-code statistical run,
-;;      adding further choice comparisons for second milestone. 
+;;      Web-server successfully configured to select players based on their teams. Finally figured out how page routing works with web-server/insta.
+;;      Only took an all-nighter :P
+;;      Moving forward. Further work is needed configuring statistics api calls to acquire more data. After that, all I'll need is to run the queries for
+;;      the required data, and display it on the application. Should talk to PK to discuss how to move forward.
+;;      Although it took forever, I'm pleased with the results of this progress. That was most of the hard part. 
 
