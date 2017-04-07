ab9cc09e3ada67ea24a7506c359f4baf3c3b8c13
Patrick <pertriquek171@gmail.com>
2017-04-06 21:00:22 -0400

    Plots with DataService procedures

--- src/testdraw.rkt
+++ src/testdraw.rkt
@@ -28,9 +28,6 @@
 
 (define stats '("REB" "AST" "STL" "BLK" "PTS"))
 
-(define Isaiah '(2.6 5.9 0.9 0.2 29.1))
-
-(define Harden '(8.1 11.2 1.5 0.5 29.2))
 
 ; (parameterize ([plot-width    150]
 ;                  [plot-height   150]
@@ -52,15 +49,18 @@
          (map vector lst1 lst2))))
 
  
-(plot (list (discrete-histogram (make-vector stats Harden)
-                                #:label "Harden stats"
-                                #:color 'red
-                                #:line-color 5)
-            (discrete-histogram (make-vector stats Isaiah)
-                                #:x-min 6
-                                #:label "Isaiah stats"
-                                #:color 2
-                                #:line-color 2)))
+;; (plot (list (discrete-histogram (make-vector stats Harden)
+;;                                #:label "Harden stats"
+;;                                #:color 'red
+;;                                #:line-color 5)
+;;            (discrete-histogram (make-vector stats Isaiah)
+;;                                #:x-min 6
+;;                                #:label "Isaiah stats"
+;;                                #:color 2
+;;#:line-color 2)))
+
+
+
 ;; Uncomment last two to plot to file
 ;; Format can't also be changed to 'pdf, 'svg, 'jpeg...
       ;#:out-file "Harden-Isaiah.png"
@@ -68,8 +68,44 @@
 
 
 
-(plot (discrete-histogram (make-vector (retrieveTeamAbrv) wins)))
+;; (plot (discrete-histogram (make-vector (retrieveTeamAbrv) wins)))
 ;; Uncomment next 2 lines to plot to file
       ;#:out-file "team-wins.png"
       ;#:out-kind 'png)
 
+
+;(retrievePlayerStats (findPlayerId (findPlayerEntry "Bradley, Avery")) #t)
+
+;(plot (list (discrete-histogram (make-vector list1 list2))))
+
+
+
+(define stats-names (cddr
+                     (retrievePlayerStats
+                      (findPlayerId
+                       (findPlayerEntry "Bradley, Avery")) #t)))
+
+(define stats-name (remove (car stats-names) stats-names))
+
+(define Isaiah (remove "2015-16" (cddr (car (retrievePlayerStats (findPlayerId (findPlayerEntry "Thomas, Isaiah")) #f)))) )
+
+ 
+;(define Isaiah '(2.6 5.9 0.9 0.2 29.1))
+
+(define Harden (remove "2015-16" (cddr (car (retrievePlayerStats (findPlayerId (findPlayerEntry "Harden, James")) #f)))))
+; (remove* '2015-16 Isaiah)
+
+(plot (list (discrete-histogram
+             (make-vector stats-name Isaiah)
+             #:skip 0.5
+             ;#:x-min 1
+             #:label "Isaiah")
+            (discrete-histogram
+             (make-vector stats-name Harden)
+             #:skip 0.5
+             ;#:x-min 1
+             #:color 2
+             #:label "harden"))
+      #:x-label "Statistics"
+      #:y-label "Values"
+      #:title "Harden vs Isaiah")
