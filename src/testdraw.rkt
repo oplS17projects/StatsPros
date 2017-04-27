3b046b04fe0f19f6333dbcd5c9344cbdffe2dddc
Patrick Kyoyetera <pertriquek171@gmail.com>
2017-04-26 21:30:24 -0400

    Before merge

--- src/testdraw.rkt
+++ src/testdraw.rkt
@@ -38,7 +38,7 @@
     (cond ((or (null? list1) (null? list2))
            (print 'done))
           (else
-           (begin (plot-single-stat (car list1) (car list2) count)
+           (begin (plot-single-stat (car list1) (car list2) count c1 c2)
                   (+ count 1)
                   (helper (cdr list1) (cdr list2) count c1 c2)))))
   (helper p1-list p2-list 0 45 76))
@@ -46,19 +46,19 @@
 
 
   (define plot-single-stat
-    (λ (v1 v2 num)
+    (λ (v1 v2 num c1 c2)
       (plot (list
              (discrete-histogram
               (list v1)
               #:skip 2.5
               #:x-min 0
-              #:color 'SlateGray
+              #:color c1
               #:label "player1")
              (discrete-histogram
               (list v2)
               #:skip 2.5
               #:x-min 1
-              #:color 'LawnGreen
+              #:color c2
               #:label "player2"))
             #:out-file (string-join (list (symbol->string (gensym)) ".png"))
             #:x-label "symbol-string stat-label"
